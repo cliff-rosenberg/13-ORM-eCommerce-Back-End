4b6f8fb0470bf2627ec06cb2f00a16bdf0f45325
Cliff Rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-06-05 20:44:37 -0700

    comments in connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,7 @@ require('dotenv').config({ path: __dirname + `/../.env` });
 
 const Sequelize = require('sequelize');
 
+// NOTE: 'JAWSDB' is for Heroku database setups
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
