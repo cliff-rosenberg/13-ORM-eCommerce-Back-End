bc204f87df769affd7b85d121d240371ea2006fc
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-06-11 20:15:30 -0700

    updated success messages

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -78,7 +78,7 @@ router.put('/:id', async (req, res) => {
         },
       }
     );
-    res.status(200).json(result);
+    res.status(200).json(result + ' tags updated');
   } catch(err) {
     res.status(400).json(err);
   }
@@ -93,7 +93,7 @@ router.delete('/:id', async (req, res) => {
         id: req.params.id,
       },
     });
-    res.status(200).json(result);
+    res.status(200).json(result + ' tags deleted');
   } catch(err) {
     res.status(400).json(err);
   }
